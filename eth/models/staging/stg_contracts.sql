5474e48f2cd7cd1e2ffd7fd154479a32c513b142
Tom Lin <theblessed918@gmail.com>
2026-08-21 14:43:28 +0800

    _source.yml config

--- eth/models/staging/stg_contracts.sql
+++ eth/models/staging/stg_contracts.sql
@@ -1,3 +1,7 @@
+-- 針對contract, token, and transaction 之間的關係介紹 https://share.gemini.google/a70LVbAVXzGL
+
+
+
 {# Best practice: 每一個連接外部資料，都應該建立一個stage table，
 讓dbt專案接下來的table，都是使用這一個stage table,
 這樣當外部資料有變動時，dbt專案的table不需要修改，只需要修改stage table的定義即可。 #}
